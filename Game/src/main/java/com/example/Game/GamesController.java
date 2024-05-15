665dc3bab4f3322befe2401f7643f8ee13462953
Mattia Moscufo <Moscufomattia3@gmail.com>
2024-05-15 20:08:15 +0200

    Aggiunto cazzate in GamesMenu.fxml, manca Table.fxml

--- Game/src/main/java/com/example/Game/GamesController.java
+++ Game/src/main/java/com/example/Game/GamesController.java
@@ -1,12 +1,12 @@
 package com.example.Game;
 
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
@@ -14,24 +14,52 @@ import java.util.ResourceBundle;
 
 public class GamesController implements Initializable {
 
+    private String[] games = {"Scopa", "Briscola", "Tressette", "Somma"};
+    private String[] cards = {"1-B", "10-D", "1-S", "10-C", "1-C", "10-B", "1-D", "10-S"};
+    private int curGame = 0;
+    private boolean isTwoPlayers = true;
+
+    @FXML
+    private ImageView cardCreate;
+
+    @FXML
+    private ImageView cardEntry;
+
+    @FXML
+    private Button createButton;
+
+    @FXML
+    private Button entryButton;
+
     @FXML
     private Label gameName;
-    private String[] games = {"Scopa","Briscola","Lorenzo","Somma"};
-    private int curGame = 0;
 
     @FXML
     private Button nextGameButton;
 
     @FXML
-    private Button previousGameButton;
+    private AnchorPane paneGames;
 
     @FXML
-    private ToggleButton playersButton;
+    private Button playersButton;
 
+    @FXML
+    private ImageView playersIcon;
+
+    @FXML
+    private Button previousGameButton;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         refreshGameNameLabel();
+        refreshPlayersButton();
+    }
+
+    @FXML
+    public void changePlayers(ActionEvent event) {
+        if (isTwoPlayers) isTwoPlayers = false;
+        else isTwoPlayers = true;
+        refreshPlayersButton();
     }
 
     @FXML
@@ -48,6 +76,9 @@ public class GamesController implements Initializable {
 
     public void refreshGameNameLabel() {
         gameName.setText(games[curGame]);
+        isTwoPlayers = true;
+        refreshPlayersButton();
+        refreshCardsImage();
 
         if (curGame == 0) previousGameButton.setVisible(false);
         else previousGameButton.setVisible(true);
@@ -55,4 +86,25 @@ public class GamesController implements Initializable {
         if (curGame == games.length - 1) nextGameButton.setVisible(false);
         else nextGameButton.setVisible(true);
     }
+
+    public void refreshPlayersButton() {
+        if (isTwoPlayers) {
+            playersButton.setText("2 giocatori");
+            playersIcon.setImage(new Image(getClass().getResource("due-dita.png").toExternalForm()));
+            playersButton.setStyle("-fx-background-color: #A92222");
+        } else {
+            playersButton.setText("4 giocatori");
+            playersIcon.setImage(new Image(getClass().getResource("quattro-dita.png").toExternalForm()));
+            playersButton.setStyle("-fx-background-color: #521A6A");
+        }
+    }
+    public void refreshCardsImage(){
+        String create = cards[curGame * 2] + ".jpg";
+        String entry = cards[curGame * 2 + 1] + ".jpg";
+
+        cardCreate.setImage(new Image(getClass().getResource("Cards_jpg/" + create).toExternalForm()));
+        cardEntry.setImage(new Image(getClass().getResource("Cards_jpg/" + entry).toExternalForm()));
+    }
 }
+
+
--- Game/src/main/java/com/example/Game/StartPageController.java
+++ Game/src/main/java/com/example/Game/StartPageController.java
@@ -3,23 +3,19 @@ package com.example.Game;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.util.Objects;
 
-public class StartPageController implements Initializable {
+public class StartPageController{
     @FXML
     private ImageView imageView;
     @FXML
@@ -28,7 +24,7 @@ public class StartPageController implements Initializable {
     @FXML
     protected void switchToScopa(ActionEvent event) throws IOException {
         Stage stage;
-        Parent root = FXMLLoader.load(getClass().getResource("GamesMenu.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("GamesMenu.fxml")));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         Scene scene = new Scene(root);
         scene.getStylesheets().add(getClass().getResource("GamesMenu.css").toExternalForm());
@@ -43,9 +39,10 @@ public class StartPageController implements Initializable {
         newSize.setSize(size.height * imageView.getFitWidth() / imageView.getFitHeight(), size.height);
         this.imageView.setFitHeight(newSize.getHeight());
         this.imageView.setFitWidth(newSize.getWidth());
-    }*/
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-    }
+    }*/
 }
+
